33b806d072cc183e00a9888a59eae6f1718b714e
Jeferson <jefersonfneves@gmail.com>
2020-06-17 14:45:19 +0100

    location schema model

---
+++ db/models.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+exports.Location = mongoose.model('Location', {
+    name: String,
+    description: String,
+    isValidate: {
+        type: Boolean,
+        default: false
+    }
+}, 'LocationJefTest');
