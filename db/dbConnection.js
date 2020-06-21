70678bf8932bd7a4bdcd4e30dc7f5ccca6d75cc8
Ragini Sonar <raginisonar@gmail.com>
2020-06-21 20:30:42 +0100

    Changes to user profile page and home page

---
+++ db/dbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+module.exports = async () => {
+  try {
+    mongoose.set("useFindAndModify", false);
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected...");
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/locations.js
+++ routes/locations.js
@@ -1,62 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const upload = require('../controllers/upload')
-const fs = require('fs');
-const path = require('path');
-locationsModels = require('../db/models').Locations;
-
+const upload = require("../controllers/upload");
+const fs = require("fs");
+const path = require("path");
+locationsModels = require("../db/models").Locations;
 
 /* GET locations listing. */
-router.get('/', function(req, res, next) {
-    res.render('locations');
+router.get("/", function (req, res, next) {
+  res.render("locations");
 });
 
+router.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render("locations", {
+        message: err,
+      });
+    } else {
+      // If no file is uploaded
+      if (req.file == undefined) {
+        res.render("locations", {
+          message: "Error: No File Selected!",
+        });
+      } else {
+        // Display image
+        res.render("locations", {
+          message: "File Uploaded Successfully!",
+          file: `/uploads/${req.file.filename}`,
+        });
 
-router.post('/upload',( req, res) => {
-    
-    upload(req, res, (err) => {
+        // Store image path in database
+        const { locationName, description } = req.body;
 
-        if(err){
-            res.render('locations', {
-                message: err
-            });
-        }else{
-            // If no file is uploaded
-            if(req.file == undefined){
-                res.render('locations',{
-                    message: 'Error: No File Selected!'
-                })
-            } else {
+        // Define a JSONobject for the image attributes for saving to database
+        var image = {
+          contentType: req.file.mimetype,
+          // Creates a new Buffer containing string (filename).
+          data: req.file.filename,
+        };
 
-                // Display image
-                res.render('locations', {
-                  message: 'File Uploaded Successfully!',
-                  file: `/uploads/${req.file.filename}` 
-                });
-                
-                // Store image path in database
-                const {locationName, description} = req.body;
+        // Include title and description
+        var item = {
+          locationName: locationName,
+          description: description,
+          image: image,
+        };
+        locationsModels.addNewLocation(item);
+        // res.redirect('/')
+      }
+    }
+  });
+});
 
-                // Define a JSONobject for the image attributes for saving to database
-                var image = {
-                    contentType: req.file.mimetype,
-                    // Creates a new Buffer containing string (filename). 
-                    data: req.file.filename
-                 };
-
-                // Include title and description
-                var item = {
-                    locationName: locationName,
-                    description: description,
-                    image: image                   
-                }
-                locationsModels.addNewLocation(item);
-                // res.redirect('/')
-          
-            }
-        }
-    })
-})
- 
-  
-  module.exports = router;
+module.exports = router;
